84c42adc702caa5c3c782d00b07da07b0354a072
fouad ajayi <fouadajayi6@gmail.com>
2024-02-22 00:26:27 +0100

    Replace the logo image and update the header navigation. (generated by blackbox.ai )

--- src/components/app/Header.tsx
+++ src/components/app/Header.tsx
@@ -1,9 +1,16 @@
-import logo from "../../assets/Logo.png" 
+import logo from "../../assets/Logo.svg" 
 
 function Header() {
   return (
-    <nav className="border-2 border-black">
+    <nav className=" flex  py-[30px] items-center px-11 justify-between  bg-white container">
         <img src={logo} alt="" />
+
+        <ul className="flex space-x-[25px] text-[18px] font-semibold leading-[24px]">
+            <li>Why Open Enterprise</li>
+            <li>Features</li>
+            <li>Contribute</li>
+            <li className="text-[#71A894]">Request early access</li>
+        </ul>
     </nav>
   )
 }
